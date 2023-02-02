aba51dcf592c881b0b3ecd51a78866fb44ead2c4
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2023-02-02 01:15:28 -0800

    Update hubbard.jl

--- examples/hubbard.jl
+++ examples/hubbard.jl
@@ -14,7 +14,11 @@ path = "../src/"
 include(path*"DMRjulia.jl")
 using .DMRJtensor
 
+
 Ns = 10
+
+@makeQNs "fermion" U1 U1
+Qlabels = [[fermion(0,0),fermion(1,1),fermion(1,-1),fermion(2,0)]]
 
 Ne = Ns
 Ne_up = ceil(Int64,div(Ne,2))
@@ -23,69 +27,42 @@ QS = 4
 Cup,Cdn,F,Nup,Ndn,Ndens,O,Id = fermionOps()
 
 psi = MPS(QS,Ns)
-upsites = [i for i = 1:Ns]
-Cupdag = Cup'
+upsites = [i for i = 1:Ne_up]
+Cupdag = Matrix(Cup')
 applyOps!(psi,upsites,Cupdag,trail=F)
 
 
-dnsites = [i for i = 1:Ns]
-Cdndag = Cdn'
+dnsites = [i for i = 1:Ne_dn]
+Cdndag = Matrix(Cdn')
 applyOps!(psi,dnsites,Cdndag,trail=F)
+
+qpsi = makeqMPS(Qlabels,psi)
 
 mu = -2.0
 HubU = 4.0
 t = 1.0
 
-function makeHubbard(Ns,t,HubU,mu,Cup,Cdn,F,Nup,Ndn,Ndens)
-
-  mpo = 0
-  for i = 1:Ns-1
-    #spin up
-    mpo += mpoterm(-t,Cup,i,Cup',i+1,F)
-    mpo += mpoterm(t,Cup',i,Cup,i+1,F)
-    #spin down
-    mpo += mpoterm(-t,Cdn,i,Cdn',i+1,F)
-    mpo += mpoterm(t,Cdn',i,Cdn,i+1,F)
-  end
-
-  onsite = mu * Ndens + HubU * Nup * Ndn
-  for i = 1:Ns
-    mpo += mpoterm(onsite,i)
-  end
-
-  return MPO(mpo)
-end
-
-mpo = makeHubbard(Ns,t,HubU,mu,Cup,Cdn,F,Nup,Ndn,Ndens)
-
-#mpo += expMPO(exp(-1/2.0),Ndens,Ndens,Ns)
-
-#=
 function H(i::Int64)
-onsite(i::Int64) = mu * Ndens + HubU * Nup * Ndn #- Ne*exp(-abs(i-Ns/2)/2)*Ndens
+        onsite = mu * Ndens + HubU * Nup * Ndn #- Ne*exp(-abs(i-Ns/2)/2)*Ndens
         return [Id  O O O O O;
             -t*Cup' O O O O O;
             conj(t)*Cup  O O O O O;
             -t*Cdn' O O O O O;
             conj(t)*Cdn  O O O O O;
-            onsite(i) Cup*F Cup'*F Cdn*F Cdn'*F Id]
+            onsite Cup*F Cup'*F Cdn*F Cdn'*F Id]
     end
-=#
-#println("Making qMPO")
-#
-#@time mpo = makeMPO(H,QS,Ns)
-#@time qmpo = makeqMPO(mpo,Qlabels)
 
-@makeQNs "fermion" U1 U1
-Qlabels = [[fermion(0,0),fermion(1,1),fermion(1,-1),fermion(2,0)]]
-qmpo,qpsi = MPO(Qlabels,mpo,psi)
+println("Making qMPO")
+
+@time mpo = makeMPO(H,QS,Ns)
+@time qmpo = makeqMPO(Qlabels,mpo)
 
 
 println("#############")
 println("QN version")
 println("#############")
 
-energyQN = dmrg(qpsi,qmpo,m=45,sweeps=20,cutoff=1E-9)
+QNenergy = dmrg(qpsi,qmpo,m=45,sweeps=20,cutoff=1E-9)
 
 
 println("#############")
@@ -94,5 +71,27 @@ println("#############")
 
 energy = dmrg(psi,mpo,m=45,sweeps=20,cutoff=1E-9)
 
-println(energyQN-energy)
+println(QNenergy-energy)
 
+
+qNup,qNdn = Qtens([Qlabels[1],inv.(Qlabels[1])],Nup,Ndn)
+
+for i = 1:length(qpsi)
+    move!(qpsi,i)
+    println(i," ",ccontract(qpsi[i],contract([2,1,3],qNup,2,qpsi[i],2)))
+  end
+  for i = 1:length(qpsi)
+    move!(qpsi,i)
+    println(i," ",ccontract(qpsi[i],contract([2,1,3],qNdn,2,qpsi[i],2)))
+  end
+
+
+
+  for i = 1:length(qpsi)
+    move!(psi,i)
+    println(i," ",ccontract(psi[i],contract([2,1,3],Nup,2,psi[i],2)))
+  end
+  for i = 1:length(psi)
+    move!(psi,i)
+    println(i," ",ccontract(psi[i],contract([2,1,3],Ndn,2,psi[i],2)))
+  end
