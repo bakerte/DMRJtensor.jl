5324468c5ba60328c1e464645b60c7656f2cd4ff
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-08-08 23:33:01 -0700

    Update alltest.jl

--- test/alltest.jl
+++ test/alltest.jl
@@ -39,7 +39,7 @@ Tests all functions in the files enumerated in `tests`. Default is to check all 
 
 See also: [`libdir`](@ref)
 """
-function libtest(;tests::Array{String,1}=tests,dir::String=libdir,path::String=dir*"/test/")
+function libtest(;tests::Array{String,1}=tests,dir::String=libdir,path::String=dir*"/../test/")
 
   fulltestrecord = Array{Bool,1}(undef,length(tests))
 
