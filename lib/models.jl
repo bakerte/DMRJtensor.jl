9f426a12f1eaf5bf6344d918e5100b3077d4f7c4
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-08-08 21:42:17 -0700

    Update models.jl

--- lib/models.jl
+++ lib/models.jl
@@ -1,12 +1,12 @@
-#########################################################################
+###############################################################################
 #
 #  Density Matrix Renormalization Group (and other methods) in julia (DMRjulia)
-#                              v0.8
+#                               v1.0
 #
-#########################################################################
-# Made by Thomas E. Baker and Jaimie Greasley (2020)
+###############################################################################
+# Made by Thomas E. Baker and « les qubits volants » (2024)
 # See accompanying license with this program
-# This code is native to the julia programming language (v1.5.4+)
+# This code is native to the julia programming language (v1.10.0+)
 #
 
 """
