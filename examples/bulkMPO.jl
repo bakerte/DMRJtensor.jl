5b9bb280d80b106d0b1c3b9f6eb14d3e07ab3086
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-08-08 21:49:09 -0700

    Create bulkMPO.jl

---
+++ examples/bulkMPO.jl
@@ -0,0 +1,27 @@
+###############################################################################
+#
+#  Density Matrix Renormalization Group (and other methods) in julia (DMRjulia)
+#                               v1.0
+#
+###############################################################################
+# Made by Thomas E. Baker and « les qubits volants » (2024)
+# See accompanying license with this program
+# This code is native to the julia programming language (v1.10.0+)
+#
+
+#This example will make the bulk MPO for the XXZ model
+#For viewing only
+
+H = bulkMPO(5)
+
+H[1,1] = H[end,end] = "I"
+
+H[2,1] = "Sz"
+H[3,1] = half*"Sp"
+H[4,1] = half*"Sm"
+
+H[end,2] = "Sz"
+H[end,3] = "Sm"
+H[end,4] = "Sp"
+
+H*H #Hamiltonian terms will accumulate in the lower left corner
